b1d7b24890a9505aa30252caf503b12b98ccd169
Mustafa Ghoneim <musgamal05@gmail.com>
2024-07-11 14:36:02 +0300

    chore: Add .idea directory to .gitignore

---
+++ test_cam.py
@@ -0,0 +1,20 @@
+import numpy as np
+from keras.models import model_from_json
+import matplotlib.pyplot as plt
+from keras.preprocessing.image import ImageDataGenerator
+from sklearn.metrics import confusion_matrix, classification_report,ConfusionMatrixDisplay
+
+
+emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
+
+# load json and create model
+json_file = open('model/emotion_model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+emotion_model = model_from_json(loaded_model_json)
+
+# load weights into new model
+emotion_model.load_weights("model/emotion_model.h5")
+print("Loaded model from disk")
+
+
